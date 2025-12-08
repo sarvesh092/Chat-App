09293593ae391a52157b4518815e4f5a22d245f5
sarvesh <kashyapsarvesh092@gmail.com>
2025-12-08 22:26:53 +0530

    added login page

---
+++ client/src/pages/Authentication/Login.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import{ Mail, KeyRound } from "lucide-react";
+
+const Login = () => {
+  const [formData, setFormData] = useState({ email: "", password: "" });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => {
+      return { ...prev, [name]: value };
+    });
+  };
+
+  console.log(formData);
+  return (
+    <div className="flex flex-col justify-center items-center p-4 min-h-screen">
+      <div className="mb-4 text-center">
+        <h2 className=" text-3xl mb-2 font-bold ">Welcome Back</h2>
+        <p className="text-sm">Please login to your account</p>
+      </div>
+
+      <form
+        className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4"
+        onSubmit={handleSubmit}
+      >
+        <div>
+          <span className="label mb-1">Email</span>
+          <label className="input validator rounded-xl">
+            <Mail />
+            <input
+              type="email"
+              placeholder="Email"
+              required
+              name="email"
+              onChange={handleChange}
+              value={formData.email}
+            />
+            <span className="validator-hint hidden">Required</span>
+          </label>
+        </div>
+
+        <div>
+          <span className="label mb-1">Password</span>
+          <label className="input validator rounded-xl">
+          <KeyRound />
+          <input
+            type="password"
+            placeholder="Password"
+            required
+            name="password"
+            onChange={handleChange}
+            value={formData.password}
+          />
+          <span className="validator-hint hidden">Required</span>
+          </label>
+        </div>
+
+        <p className="mt-2">
+          Don't have an account? <NavLink to="/signup">Register</NavLink>
+        </p>
+
+        <button
+          className="btn btn-neutral mt-4 w-full rounded-xl"
+          type="submit"
+        >
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
